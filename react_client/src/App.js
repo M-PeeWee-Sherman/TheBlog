12ee87a82fba7646f34d321b5993e3be951a9799
PeeWee <AlphaSherman@gmail.com>
2022-05-29 17:12:57 -0400

    React Post can edit items; post submit does not work

--- react_client/src/App.js
+++ react_client/src/App.js
@@ -1,21 +1,29 @@
-import logo from './logo.svg';
+
 import useBloglist from './customHooks/useBlogList'
 
 import PostList from './Components/PostList'
 import './App.css';
 
 function App() {
-  const [fullList, setFullList, updateFn] = useBloglist([]);
+  const [fullList, setFullList, updateFn] = useBloglist();
   let temp = {id:'1', users_id: '1',stamp:'10 Mar 2022',title:'alpha Intro',content:'alpha Intro Content \nxxxxxxxxxxxxxxxxxxxxxxx'};
   let list = [];
   for (let i=0; i < 10; i++){
       list.push({...temp});
   }
   let blank = ()=>{};
-  
+  let submitUpdate = ({id, users_id, content, title})=>{
+    const stamp = new Date().toUTCString();
+    fetch("http://localhost:3001/posts", {
+      method: "POST",
+      headers: {'Content-Type': 'application/json'}, 
+      body: JSON.stringify({id, users_id,stamp,title,content})
+    })
+      .then(updateFn());
+  }
   return (
     <div className="App">
-      <PostList updateFn={updateFn} deleteFn={blank} entryList={fullList}/>
+      <PostList updateFn={submitUpdate} deleteFn={blank} entryList={fullList}/>
     </div>
   );
 }
--- react_client/src/Components/Post.js
+++ react_client/src/Components/Post.js
@@ -2,6 +2,7 @@ import React, {  useState } from "react";
 import Typography from '@mui/material/Typography';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
+import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
 
 const pads = {
@@ -21,7 +22,15 @@ const Post=({updateFn,deleteFn,entry})=> {
     const {id, users_id, stamp, title, content}=entry;
     const [titleState, setTitleState] = useState(title);
     const [contentState, setContentState] = useState(content);
+    const editFields = [{label:"Edit",readOnly:true,display:"none"},{label:"Cancel",readOnly:false,display:"block"}]
+    const [editState, setEditState] = useState(0);
 
+    let handleInputTitleChange=(e)=>{
+        setTitleState(e.target.value);
+    }
+    let handleInputContentChange=(e)=>{
+        setContentState(e.target.value);
+    }
 
     return (<Grid
             container
@@ -37,33 +46,54 @@ const Post=({updateFn,deleteFn,entry})=> {
                 alignItems="center"
                 > 
                     <TextField
-                    id="title"
+                    id={`title_${id}`}
                     label="Title"
-                    defaultValue={titleState}
+                    onChange={handleInputTitleChange}
+                    value={titleState}
                     sx={{width:'68%'}}
                     InputProps={{
-                        readOnly: true,
+                        readOnly: editFields[editState].readOnly,
                     }}/>
                     <Typography sx={{width:'28%'}}>{stamp}</Typography>        
                 </Grid> 
                         
                 <TextField 
-                    id="content" 
+                    id={`content_${id}`}
                     label="Content" 
                     multiline
                     fillWidth
+                    onChange={handleInputContentChange}
                     sx={{...pads, width: '90%'}}
                     variant="outlined"                     
-                    defaultValue={contentState}
+                    value={contentState}
                     InputProps={{
-                        readOnly: true,
+                        readOnly: editFields[editState].readOnly,
                     }}
                     />
                 <Grid container direction="row" 
                     justifyContent="center"
                     alignItems="center"> 
                     <Button>Expand</Button>
-                    <Button>Edit</Button>
+                    <Button id={`EditButton_${id}`} 
+                        onClick={(e)=>{
+                        e.preventDefault();
+                        
+                        if(editState===0){
+                            setEditState(1)
+                        }else{
+                            setContentState(content);
+                            setTitleState(title);
+                            setEditState(0);
+                        }
+                    }}>{editFields[editState].label}</Button>
+                    <Box id={`SubmitBox_${id}`} component="span" display={editFields[editState].display}>
+                        <Button 
+                            id={`submitUpdateButton_${id}`} 
+                            onClick={(e)=>{
+                                e.preventDefault();
+                                updateFn({id, users_id, content:contentState, title:titleState});
+                            }}
+                            >Submit</Button></Box>
                     <Button>Delete</Button> 
                 </Grid>
             </Grid>)
